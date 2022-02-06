b486a808fcd62812ab1bc075881af091e8f58335
EugeneGpil <gpilligrim@gmail.com>
2022-02-06 20:49:34 +0700

    immersive mode

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -13,9 +13,8 @@
 
 <script lang="ts">
 import Vue from "nativescript-vue"
-
-import {android as androidApp} from '@nativescript/core/application';
-import {device} from '@nativescript/core/platform';
+import {android as androidApp} from '@nativescript/core/application'
+import {device} from '@nativescript/core/platform'
 
 declare var android: any;
 
@@ -28,14 +27,14 @@ export default Vue.extend({
 
   mounted(): void {
     if (androidApp && device.sdkVersion >= '21') {
-      const View = android.view.View;
-      const window = androidApp.startActivity.getWindow();
-      const decorView = window.getDecorView();
+      const View = android.view.View
+      const window = androidApp.startActivity.getWindow()
+      const decorView = window.getDecorView()
       decorView.setSystemUiVisibility(
           View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
           View.SYSTEM_UI_FLAG_FULLSCREEN |
           View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-      );
+      )
     }
   },
 })
